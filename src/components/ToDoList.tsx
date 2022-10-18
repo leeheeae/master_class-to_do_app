8bb052263aceae19ef99c07b3fc8a36b2e99aa0e
Heeae <chromeee@daum.net>
2022-10-18 23:32:25 +0900

    recoil을 이용한 todos

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,52 @@
+import { useForm } from "react-hook-form";
+import { atom, useRecoilState } from "recoil";
+
+interface IForm {
+  toDo: string;
+}
+
+interface IToDo {
+  id: number;
+  text: string;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
+const toDoState = atom<IToDo[]>({
+  key: "toDo",
+  default: [],
+});
+
+function ToDoList() {
+  const [toDos, setToDos] = useRecoilState(toDoState); // 읽기/쓰기 전부 사용할 때
+  const { register, handleSubmit, setValue } = useForm<IForm>();
+  const handleValid = ({ toDo }: IForm) => {
+    setToDos((oldToDos) => [
+      { id: Date.now(), text: toDo, category: "TO_DO" },
+      ...oldToDos,
+    ]);
+    setValue("toDo", "");
+  };
+
+  console.log(toDos);
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(handleValid)}>
+        <input
+          placeholder="write a to do"
+          {...register("toDo", {
+            required: "Please write a To Do",
+          })}
+        />
+        <button>Add</button>
+      </form>
+      <ul>
+        {toDos.map((toDo) => (
+          <li key={toDo.id}>{toDo.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ToDoList;
